18971bb74c91aea6ad36517dcfee52165325cf04
havr1985 <gavrtel1985@gmail.com>
2023-08-03 20:28:23 +0300

    task 2 finfished

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const KEY_TIME = "currentTime";
+
+const onPlay = function (data) {
+    console.log(data.seconds);
+    localStorage.setItem(KEY_TIME, JSON.stringify(data.seconds));
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+player.setCurrentTime(JSON.parse(localStorage.getItem(KEY_TIME)) || 0);
 
