75b678e107916c3e4a8c8b358185802d37dbbfff
havr1985 <gavrtel1985@gmail.com>
2023-08-04 13:40:36 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(userInput, 500));
+form.addEventListener('submit', onSubmit);
+
+const KEY_INPUT = "feedback-form-state";
+const formData = JSON.parse(localStorage.getItem(KEY_INPUT)) || {};
+
+chekIputs();
+
+function userInput(event) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(KEY_INPUT, JSON.stringify(formData));
+};
+
+function chekIputs() {
+    const { email, message } = form.elements;
+    if (formData) {
+        email.value = formData.email || "";
+        message.value = formData.message || "";
+    }
+}
+
+function onSubmit(event) {
+    event.preventDefault();
+    if (!(form.email.value) || !(form.message.value)) {
+        alert("Не всі поля заповнені!!!");
+        return;
+    };
+    console.log(formData);
+    event.currentTarget.reset();
+    localStorage.removeItem(KEY_INPUT);
+}
